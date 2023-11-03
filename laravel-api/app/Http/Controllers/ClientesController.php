666a19b39c1f791e05b8d78da1d80056d7a485e7
Renan Lacerda <renanrodlacerda@gmail.com>
2023-11-03 11:06:09 -0300

    feat: inserir cliente no banco

---
+++ laravel-api/app/Http/Controllers/ClientesController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Models\Clientes;
+
+class ClientesController extends Controller
+{
+    public function inserirCliente(Request $request) {
+
+        $nome = strtolower($request->input('nome'));
+        $email = strtolower($request->input('email'));
+
+        try {
+            $cliente = Clientes::create([
+                'nome' => $nome,
+                'email' => $email
+            ]);
+
+            return response()->json(['id' => $cliente->id]);
+        } catch(\Illuminate\Database\QueryException $error) {
+            if ($error->errorInfo[1] === 1062) {
+                return response()->json(['error' => 'Email já cadastrado.'], 422);
+            }
+        }
+    }
+}
